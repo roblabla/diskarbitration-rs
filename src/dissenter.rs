a4fa66853b1087095026042e558f1d54255ff594
roblabla <robinlambertz+dev@gmail.com>
2016-12-14 13:14:01 +0100

    Add functions DADissenterGetStatus and DADissenterGetStatusString

--- src/dissenter.rs
+++ src/dissenter.rs
@@ -1,5 +1,13 @@
-use libc::c_void;
+use libc::{c_void, c_int};
+use core_foundation_sys::string::CFStringRef;
 
 #[repr(C)]
 pub struct __DADissenter(c_void);
 pub type DADissenterRef = *const __DADissenter;
+
+pub type DAReturn = c_int;
+
+extern {
+    pub fn DADissenterGetStatus(dissenter: DADissenterRef) -> DAReturn;
+    pub fn DADissenterGetStatusString(dissenter: DADissenterRef) -> CFStringRef;
+}
