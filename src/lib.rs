5480abe114c31e04b1dce6e5f42996a23830190f
Dilawar Singh <dilawar@subcom.tech>
2023-08-03 15:26:15 +0530

    doc: Adds links to Apple Dev page

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! Bindings to Apple's Disk Arbitration API
+//!
+//! For more information, see
+//! <https://developer.apple.com/library/archive/documentation/DriversKernelHardware/Conceptual/DiskArbitrationProgGuide/Introduction/Introduction.html>
+
 #![allow(non_upper_case_globals)]
 
 pub mod base;
