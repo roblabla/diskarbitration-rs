ddd134515fc6560b0527969e4f9f69e30558f250
Dilawar Singh <dilawar@subcom.tech>
2023-08-03 15:14:52 +0530

    chore: edition 2021
    And rustfmt

--- src/disk.rs
+++ src/disk.rs
@@ -1,16 +1,18 @@
-use libc::{c_void, c_char};
 use core_foundation_sys::base::CFAllocatorRef;
 use core_foundation_sys::url::CFURLRef;
+use libc::{c_char, c_void};
 
-use session::DASessionRef;
+use crate::session::DASessionRef;
 
 #[repr(C)]
 pub struct __DADisk(c_void);
 pub type DADiskRef = *const __DADisk;
 
-extern {
-    pub fn DADiskCreateFromVolumePath(allocator: CFAllocatorRef,
-                                      session: DASessionRef, path: CFURLRef)
-    -> DADiskRef;
+extern "C" {
+    pub fn DADiskCreateFromVolumePath(
+        allocator: CFAllocatorRef,
+        session: DASessionRef,
+        path: CFURLRef,
+    ) -> DADiskRef;
     pub fn DADiskGetBSDName(disk: DADiskRef) -> *const c_char;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![allow(non_upper_case_globals)]
 
-extern crate core_foundation_sys;
-extern crate libc;
-
 pub mod base;
 pub mod session;
 pub mod disk;
